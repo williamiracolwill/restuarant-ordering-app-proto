b4de93061e9dae20e9ab77bfd3c4fae7ca91da9b
williamiracolwill <williamiracolwill@gmail.com>
2025-10-21 14:05:43 -0600

    Refactored how the js file was uploading the html to page. Added EventListener to gain access to each item when add item btn is clicked

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import { menuArray } from "./data.js";
 const menuList = document.getElementById('food-items');
 
+menuList.addEventListener('click', function(e) {
+    if (e.target.dataset.pizza) {
+        console.log(e.target.dataset.pizza)
+    } else if (e.target.dataset.hamburger) {
+        console.log(e.target.dataset.hamburger)
+    } else if (e.target.dataset.beer) {
+        console.log(e.target.dataset.beer)
+    }
+});
 
-const menuElements = menuArray.map(item => {
-    return `<div class="item-container">
+
+
+function getMenuHtml() {
+    return menuArray.map(item => `
+        <div class="item-container">
                 <div class="item-info">
                     <span class="item-emoji">${item.emoji}</span>
                     <div>
@@ -13,9 +25,15 @@ const menuElements = menuArray.map(item => {
                     </div>
                 </div>
                 <button class="add-item-btn">
-                    <img src="assets/add-btn.png" alt="Add" />
+                    <img src="assets/add-btn.png" alt="Add" data-${item.name}="${item.id}" />
                 </button>
             </div>`
-});
+        ).join('');
+};
 
-menuList.innerHTML = menuElements.join('');
+
+function render() {
+    menuList.innerHTML = getMenuHtml();
+}
+
+render();
