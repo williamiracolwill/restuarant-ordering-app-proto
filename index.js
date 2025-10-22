6c4918e3e591990c89251e46e6f29ef1de23fe61
williamiracolwill <williamiracolwill@gmail.com>
2025-10-22 11:54:11 -0600

    Added funcionality to adding items to your order, can now add and remove items with total price updating each time

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 import { menuArray } from "./data.js";
 
 const menuList = document.getElementById('menu');
+const addedItemsList = document.getElementById('added-items-list')
 
-menuList.addEventListener('click', function(e) {
+
+document.addEventListener('click', function(e) {
     if (e.target.dataset.pizza) {
         handleAddItem(e.target.dataset.pizza)
     } else if (e.target.dataset.hamburger) {
         handleAddItem(e.target.dataset.hamburger)
     } else if (e.target.dataset.beer) {
         handleAddItem(e.target.dataset.beer)
+    } else if (e.target.dataset.removeBtn) {
+        handleRemoveItem(e.target.dataset.removeBtn)
     }
 });
 
 const myOrderItems = []
 
 function handleAddItem(itemId) {
-    console.log(itemId)
+    const targetItemObj = menuArray.filter(menuItem => menuItem.id === Number(itemId))[0]
+    myOrderItems.push(targetItemObj)
+    updateTotalPrice()
+    render()
+}
+
+function handleRemoveItem(itemId) {
+    const index = myOrderItems.findIndex(obj => obj.id === Number(itemId))
+    if (index !== -1) {
+        myOrderItems.splice(index, 1);
+    }
+    updateTotalPrice()
+    render()
+}
+
+function getOrderHtml() {
+    return myOrderItems.map(item => `
+        <li class="list-item display-flex">
+            <div class="display-flex">
+                <h3>${item.name}</h3> 
+                <button 
+                    id="remove-btn-${item.id}" 
+                    class="remove-btn"
+                    data-remove-btn="${item.id}"
+                    >remove</button>
+            </div>
+            <p>$${item.price}</p>
+        </li>`).join('')
+}
+
+function updateTotalPrice() {
+    const totalPrice = document.getElementById('total-price')
+    const total = myOrderItems.reduce((sum, item) => sum + item.price, 0)
+    totalPrice.textContent = `$${total}`
 }
 
 
@@ -40,6 +77,7 @@ function getMenuHtml() {
 
 function render() {
     menuList.innerHTML = getMenuHtml();
+    addedItemsList.innerHTML = getOrderHtml()
 }
 
 render();
